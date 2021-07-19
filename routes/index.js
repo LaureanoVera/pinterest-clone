7d9ffc626953e9ea60ab9cf52419a78c8060417d
LaureanoVera <laureanoigvera@gmail.com>
2021-07-19 10:35:14 -0300

    estructura de routes

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const { Router } = require('express')
+const router = Router()
+
+router.get('/', (req, res) => {
+  res.send('Index Page')
+})
+
+router.get('/upload', (req, res) => {
+  res.send('Form Upload')
+})
+
+router.post('/upload', (req, res) => {
+  res.send('Uploaded')
+})
+
+router.get('/image/:id', (req, res) => {
+  res.send('Profile')
+})
+
+router.get('/image/:id/delete', (req, res) => {
+  res.send('Image Delete')
+})
+
+module.exports = router
