e940591fdf856bc513925dd68fffbbb8c0ade751
LaureanoVera <laureanoigvera@gmail.com>
2021-07-19 10:35:05 -0300

    estructura de routes

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const path = require('path')
+const express = require("express");
+const app = express();
+const path = require("path");
+const morgan = require("morgan");
+const multer = require("multer");
 
 // SETTINGS
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+const port = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // STATIC
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 // MIDDLEWARES
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  multer({
+    dest: path.join(__dirname, "public", "images", "upload"),
+  }).single("image")
+);
 
 // GLOBAL
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.send('INDEX')
-})
+app.use(require('./routes/index'))
 
 // SERVER
 app.listen(port, () => {
   console.log(`Server listened on port ${port}`);
-})
+});
